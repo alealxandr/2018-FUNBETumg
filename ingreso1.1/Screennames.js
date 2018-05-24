b4ea012bc4fdaee42f6f4139fcf01b6100190460
alealxandr <alealxandr@gmail.com>
2018-05-23 20:44:06 -0600

    version 1.1

---
+++ ingreso1.1/Screennames.js
@@ -0,0 +1,23 @@
+const LoginScreen= "LoginScreen";
+ const LigaScreen= "LigaScreen";
+ const bdScreen= "bdScreen";
+ const perfilScreen="perfilScreen";
+ const calendarioScreen="calendarioScreen";
+ const infoScreen="infoScreen";
+ const equipoScreen="equipoScreen";
+ const pagoScreen="pagoScreen";
+ const miPerfilStack="miPerfilStack";
+ const loginStack="loginStack";
+
+ export{
+   LoginScreen,
+   LigaScreen,
+   bdScreen,
+   perfilScreen,
+   calendarioScreen,
+   infoScreen,
+   equipoScreen,
+   pagoScreen,
+   loginStack,
+   miPerfilStack
+ };
